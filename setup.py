2c1e6b92aa48dbbd2b9178e8052f4fe95fe25700
Matt Snider <matt.snider@alum.utoronto.ca>
2017-01-29 18:16:48 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='celery-scheduler',
+    version='0.1',
+    description='Celery scheduler supporting various backends',
+    author='Matt Snider',
+    author_email='matt.snider@alum.utoronto.ca',
+    url='https://www.github.com/matt-snider/celery-scheduler',
+    packages=['celery_scheduler'],
+)
